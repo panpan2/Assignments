019d5f80cf09f898db73ca7e42e991d0930d86c0
panpan2 <p.panayiotou2@gmail.com>
2016-07-15 11:27:52 +0300

    [task3] Added Register/Thanks views

---
+++ task3/task3/resources/views/users.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Register</title>
+    </head>
+    <body>
+        <form id='register'>
+            Name: <input type="text" name="name"><br>
+            Email: <input type="text" name="Email"><br>
+            <input type="submit" value="Register">
+        </form>
+    </body>
+</html>
