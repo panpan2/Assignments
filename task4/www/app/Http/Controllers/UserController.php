ab918eaec04cd44386d4a43f79b9e7c49dd53425
panpan2 <p.panayiotou2@gmail.com>
2016-07-28 12:59:28 +0300

    [task4] Setup user model, controller and database migrations

---
+++ task4/www/app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class UserController extends Controller
+{
+    /**
+     *
+     * Responds to POST requests of /register
+     * Registers user if the form input is valid
+     * and redirects to "Thank you" page
+     *
+     */
+    public function postRegister(Request $request)
+    {
+        $this->validate(
+            $request, [
+                'name' => 'required|max:255|string|alpha',
+                'email' => 'required|unique:users,email|max:255|string|email',
+                'phone' => 'required|max:255|string|integer',
+            ]
+        );
+        // form input was vali
+        $user = new User();
+        $user->email = $request->email;
+        $user->name = $request->name;
+        $user->phone = $request->phone;
+        $user->save();
+        return redirect('thankyou');
+    }
+}
--- task4/www/app/User.php
+++ task4/www/app/User.php
@@ -2,28 +2,24 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use Notifiable;
+    /**
+     *
+     * The table associated with the model
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
     /**
-     * The attributes that are mass assignable.
      *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
+     * Indicates if the model should be timestamped
      *
-     * @var array
+     * @var bool
+     *
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    public $timestamps = false;
 }
